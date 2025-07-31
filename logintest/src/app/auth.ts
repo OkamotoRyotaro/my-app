3d41bbfc30badc7ed78b939c618b298ddb3b3447
RYOTARO <okamotoryo@yamaha-robotics.com>
2025-07-31 15:52:28 +0900

    session kill

--- logintest/src/app/auth.ts
+++ logintest/src/app/auth.ts
@@ -1,22 +1,15 @@
+
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
+import LinkedIn from "next-auth/providers/linkedin";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google,
+    LinkedIn,
   ],
-  callbacks: {
-    async signIn({ user }) {
-      if (!user?.email) return false;
-      // DBから許可メールアドレスを取得
-      const allowed = await prisma.allowList.findUnique({
-        where: { email: user.email },
-      });
-      return !!allowed;
-    },
+  session: {
+    strategy: "jwt",
+    maxAge: 30, // 30s
   },
 });
--- logintest/src/app/login/page.tsx
+++ logintest/src/app/login/page.tsx
@@ -1,39 +1,29 @@
 import { signIn, auth, signOut } from "@/app/auth"
 import Link from "next/link";
 
+import { redirect } from "next/navigation";
+
 export default async function Login() {
   const session = await auth();
 
-  console.log(session)
+  if (!session) {
+    // セッションがなければログインページ（Googleサインイン）へリダイレクト
+    redirect("/api/auth/signin");
+  }
+
   return (
     <>
-      {session !== null ? (
-        <>
-          <Link href={"/dashboard"}>dashboard</Link>
-          <h1>{session.user?.name}がログインしたよ</h1>
-          <img src={session.user?.image as string} alt="user image" />
-          <form
-            action={async () => {
-              "use server";
-              await signOut();
-            }}
-          >
-            <button type="submit">Signout</button>
-          </form>
-        </>
-      ) : (
-        <>
-          <h1>ログインしてね</h1>
-          <form
-            action={async () => {
-              "use server";
-              await signIn("google");
-            }}
-          >
-            <button type="submit">Signin with Google</button>
-          </form>
-        </>
-      )}
+      <Link href={"/dashboard"}>dashboard</Link>
+      <h1>{session.user?.name}がログインしたよ</h1>
+      <img src={session.user?.image as string} alt="user image" />
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button type="submit">Signout</button>
+      </form>
     </>
-  )
+  );
 }
