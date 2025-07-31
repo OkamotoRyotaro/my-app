4e080d388b875f1f227034cb56b4bffe8e70b132
RYOTARO <okamotoryo@yamaha-robotics.com>
2025-07-31 16:10:01 +0900

    ref email

--- logintest/src/app/dashboard/page.tsx
+++ logintest/src/app/dashboard/page.tsx
@@ -1,10 +1,15 @@
-import { auth } from "@/app/auth"
-import { redirect } from 'next/navigation';
+import { auth } from "@/app/auth";
+import { redirect } from "next/navigation";
+
+//TODO notion に登録した人のメールアドレスを登録して、参照すれば有料コンテンツの表示ができそう
+const allowedEmails = [
+  "o.ryotaro23@gmail.com", // 閲覧を許可するメールアドレスをここに追加
+];
 
 export default async function Dashboard() {
   const session = await auth();
-  if (!session) {
-    redirect('/'); // 未ログインならリダイレクト
+  if (!session || !session.user?.email || !allowedEmails.includes(session.user.email)) {
+    redirect("/"); // 許可されていない場合はトップページへリダイレクト
   }
 
   return (
