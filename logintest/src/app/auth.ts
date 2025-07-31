7b0c3f45f52b0298deb95f22f450208777d22b6f
RYOTARO <okamotoryo@yamaha-robotics.com>
2025-07-31 16:21:23 +0900

    display sign in log

--- logintest/src/app/auth.ts
+++ logintest/src/app/auth.ts
@@ -12,4 +12,12 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     strategy: "jwt",
     maxAge: 30, // 30s
   },
+  callbacks: {
+    async signIn({ user }) {
+      //TODO log file を書いても良さそう
+      console.log("Google認証ユーザー:", user);
+      // 必要ならDBや外部サービスに記録も可能
+      return true;
+    },
+  },
 });
